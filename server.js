c79be762e9918367298561e1222309f950cd4a40
Tvoir <TommyVoir@yahoo.com>
2022-09-15 18:46:23 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT
 // console.log(PORT)
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 //ROUTES 
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
@@ -14,12 +19,13 @@ app.get('/', (req, res) => {
 //Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 //LISTEN 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 })
-
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
