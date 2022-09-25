97980ab2579525a3725d1d16da818bf4cb642099
Tvoir <TommyVoir@yahoo.com>
2022-09-25 11:39:13 -0700

    customer helper pt finished

--- models/bread.js
+++ models/bread.js
@@ -4,16 +4,9 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose 
 
 
-
-
 const breadSchema = new Schema({
-  // {
-  //   name: 'Rye',
-  //   hasGluten: true,
-  //   image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?
-  //   ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8
-  //   fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-  // }
+  
+
   name: { type: String, required: true },
   hasGluten: Boolean,
   image: { type: String, default: 'http://placehold.it/500x500.png' },
@@ -22,6 +15,11 @@ const breadSchema = new Schema({
     enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
   }
 })
+// helper methods 
+breadSchema.methods.getBakedBy = function(){
+  return `${this.name} was baked with love by ${this.baker}`
+}
+
 
 const Bread = mongoose.model('Bread', breadSchema)
 module.exports = Bread
