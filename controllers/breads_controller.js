bc67441ec6f991d815dbcb40434a58022eb4a35e
Tvoir <TommyVoir@yahoo.com>
2022-09-25 10:25:51 -0700

    helper methods done!

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,12 +4,14 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index', 
-        {
-            breads:Bread
-        }
-    )
-  // res.send(Bread)
+  Bread.find()
+      .then(foundBreads => {
+        res.render('index',
+          {
+            breads: foundBreads,
+            title: 'Index Page'
+          })
+      })
 })
 
 module.exports = breads
@@ -19,11 +21,24 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .then(foundBread => {
+          res.render('show', {
+              bread: foundBread
+          })
+      })
+})
+
 // EDIT
 breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
+  Bread.findById(req.params.indexArray)
+  .then(foundBread => {
+    res.render('edit', {
+      bread: foundBread
+  })
+    // bread: Bread[req.params.indexArray],
+    // index: req.params.indexArray
   })
 })
 
@@ -40,6 +55,7 @@ breads.get('/:arrayIndex', (req, res) => {
   }
 })
 
+
 // UPDATE
 breads.put('/:arrayIndex', (req, res) => {
   if(req.body.hasGluten === 'on'){
@@ -47,8 +63,11 @@ breads.put('/:arrayIndex', (req, res) => {
   } else {
     req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate (req.params.arrayIndex, req.body, { new: true})
+    .then(updatedBread => {
+      console.log(updatedBread)
+        res.redirect(`/breads/${req.params.arrayIndex}`)
+    })
 })
 
 
@@ -56,21 +75,23 @@ breads.put('/:arrayIndex', (req, res) => {
 // CREATE
 breads.post('/', (req, res) => {
   if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    req.body.image = undefined
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread.push(req.body)
+  Bread.create(req.body)
   res.redirect('/breads')
 })
 
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+  Bread.findByIdAndDelete (req.params.indexArray)
+    .then(deletedBread => {
+      res.status(303).redirect('/breads')
+    })
 })
 
 
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
 // DEPENDENCIES
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
+
+
 
 //CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 // console.log(PORT)
 const app = express()
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
